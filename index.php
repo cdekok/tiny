a9ef2d8417c7a9e8dc902efa7d4624261e540be5
Chris de Kok <chris.de.kok@gmail.com>
2014-10-29 22:34:36 +0100

    Add spiffy router

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-
-$router = new \Tiny\Router();
+require_once 'vendor/autoload.php';
+$router = new \Spiffy\Route\Router;
 $router->add('foo', '/foo/{name}');
 $route = $router->match($_SERVER['PATH_INFO']);
 var_dump($route->getParams()['name']);
