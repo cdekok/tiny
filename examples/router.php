273b703b5afcd1484b97404c167fc756b18d3b51
Chris de Kok <chris.de.kok@gmail.com>
2014-10-29 21:54:42 +0100

    Add request

--- examples/router.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$router = new \Tiny\Router();
-
-$router->add('foo', '/foo/{name}');
-var_dump($router->assemble('foo', ['name' => 'test']));
-var_dump($router->match('/foo/test'));
--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
 $router = new \Tiny\Router();
-
 $router->add('foo', '/foo/{name}');
-var_dump($router->assemble('foo', ['name' => 'test']));
-var_dump($router->match('/foo/test'));
+$route = $router->match($_SERVER['PATH_INFO']);
+var_dump($route->getParams()['name']);
